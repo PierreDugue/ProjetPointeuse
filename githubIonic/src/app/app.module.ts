38c21e8adf773f64dc114dae127340641c3cbec5
Pierre Dugue <pierre.dugue1@gmail.com>
2016-11-29 15:46:39 +0100

    Reinitialisation du projet

--- githubIonic/src/app/app.module.ts
+++ githubIonic/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { MyApp } from './app.component';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
+import { ValidationPage } from '../pages/validation/validation';
 
 @NgModule({
   declarations: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    ValidationPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,7 +22,8 @@ import { ListPage } from '../pages/list/list';
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    ValidationPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
